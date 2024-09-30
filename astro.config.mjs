3d1282d257b8875d5934c44a6a2371f4bf4a4964
jwar28 <javierguerra2203@gmail.com>
2024-09-30 16:41:26 -0500

    refactor: some layout changes added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
-
-import vercel from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
+	output: "server",
+	adapter: vercel(),
 });
